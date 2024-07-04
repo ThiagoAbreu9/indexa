e9a3f1df951d50eaf416c905d06ce21a5e9ef928
Thiago <tas3@icomp.ufam.edu.br>
2024-07-04 00:00:26 -0400

    Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatosComponent } from './componentes/contatos/contatos.component';
 
+interface Contato{
+  id : number;
+  nome : string;
+  telefone : string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent, 
+    SeparadorComponent,
+    ContatosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string ='abcdefghijklmnopqrstuvxwyz'
+
 }
---
+++ src/app/componentes/contatos/contatos.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos',
+  standalone: true,
+  imports: [],
+  templateUrl: './contatos.component.html',
+  styleUrl: './contatos.component.css'
+})
+export class ContatosComponent {
+  @Input() nome: string=''
+  @Input() telefone: string=''
+}
